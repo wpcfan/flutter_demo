76e39fe3c9b1fd99a1976dfcf9fa56750dae97a1
Peng <wpcfan@gmail.com>
2022-11-02 20:50:44 +0800

    add forms

---
+++ lib/forms/login_form_widget.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+class LoginFormWidget extends StatefulWidget {
+  const LoginFormWidget({super.key});
+
+  @override
+  State<LoginFormWidget> createState() => _LoginFormWidgetState();
+}
+
+class _LoginFormWidgetState extends State<LoginFormWidget> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  bool _passwordVisible = false;
+  bool _usernameHasError = false;
+  bool _passwordHasError = false;
+  @override
+  Widget build(BuildContext context) {
+    return form();
+  }
+
+  /// 用户名输入框
+  Widget usernameFormField() {
+    return FormBuilderTextField(
+      name: 'username',
+      decoration: InputDecoration(
+        labelText: 'Username',
+        prefix: _usernameHasError
+            ? const Icon(Icons.error, color: Colors.red)
+            : const Icon(Icons.check, color: Colors.green),
+      ),
+      onChanged: (val) => setState(() => _usernameHasError =
+          !(_formKey.currentState?.fields['username']?.validate() ?? false)),
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        FormBuilderValidators.minLength(3),
+      ]),
+    );
+  }
+
+  /// 密码输入框
+  Widget passwordFormField() {
+    return FormBuilderTextField(
+      name: 'password',
+      obscureText: !_passwordVisible,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        prefix: _passwordHasError
+            ? const Icon(Icons.error, color: Colors.red)
+            : const Icon(Icons.check, color: Colors.green),
+        suffix: IconButton(
+          icon: _passwordVisible
+              ? const Icon(Icons.remove_red_eye)
+              : const Icon(Icons.remove_red_eye_outlined),
+          onPressed: () => setState(() => _passwordVisible = !_passwordVisible),
+        ),
+      ),
+      onChanged: (val) => setState(() => _passwordHasError =
+          !(_formKey.currentState?.fields['password']?.validate() ?? false)),
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        FormBuilderValidators.minLength(3),
+      ]),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState?.saveAndValidate() ?? false) {
+          debugPrint(_formKey.currentState?.value.toString());
+        } else {
+          debugPrint(_formKey.currentState?.value.toString());
+          debugPrint('validation failed');
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0.0),
+        ),
+      ),
+      child: const Text(
+        'Submit',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget resetButton() {
+    return OutlinedButton(
+      onPressed: () {
+        _formKey.currentState?.reset();
+      },
+      // color: Theme.of(context).colorScheme.secondary,
+      child: Text(
+        'Reset',
+        style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+      ),
+    );
+  }
+
+  /// 构建登录表单
+  Widget form() {
+    return Center(
+        child: Column(
+      children: [
+        FormBuilder(
+          key: _formKey,
+          onChanged: () {
+            _formKey.currentState!.save();
+            debugPrint(_formKey.currentState!.value.toString());
+          },
+          autovalidateMode: AutovalidateMode.disabled,
+          initialValue: const {
+            'username': '',
+            'password': '',
+          },
+          skipDisabled: true,
+          child: Column(
+            children: [
+              usernameFormField(),
+              passwordFormField(),
+            ],
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: submitButton(),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: resetButton(),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,23 +1,16 @@
+import 'package:demo/forms/login_form_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final authService = Provider.of<AuthService>(context);
     return Scaffold(
       appBar: AppBar(
-          // title: Text(APP_PAGE.login.toTitle),
-          ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            // authService.login();
-          },
-          child: const Text("Log in"),
-        ),
+        title: const Text('Login'),
       ),
+      body: const LoginFormWidget(),
     );
   }
 }
